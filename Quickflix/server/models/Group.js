7ca4287af81799ab6394e1d7804a5f3d904e628c
JD Simmons <hippeejd@gmail.com>
2022-08-31 12:21:18 -0600

    some

---
+++ Quickflix/server/models/Group.js
@@ -0,0 +1,18 @@
+import {Schema} from "mongoose";
+const ObjectId = Schema.Types.ObjectId
+
+
+export const GroupSchema = new Schema({
+name: {type: String, required: true, minlength: 1, maxlength: 100},
+creatorId: { type: ObjectId, required: true, ref: 'Account'}
+},
+{timestamps: true, toJSON: {virtuals:true}}
+
+)
+
+GroupSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: ' _id',
+  justOne: true,
+  ref: 'Account'
+})
