d6e94938147167a9855aadf7abe1166cdf5dcbb7
Curtis Pollard <clp.the.second@gmail.com>
2022-08-31 13:31:45 -0600

    Making changes

--- Quickflix/server/db/DbContext.js
+++ Quickflix/server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { ContentSchema } from '../models/Content';
 import { GroupSchema } from "../models/group";
 import { ValueSchema } from '../models/Value'
 class DbContext {
@@ -7,6 +8,8 @@ class DbContext {
   Account = mongoose.model('Account', AccountSchema);
 
   Groups = mongoose.model('Group', GroupSchema);
+
+  Content = mongoose.model('Content', ContentSchema);
 }
 
 export const dbContext = new DbContext()
