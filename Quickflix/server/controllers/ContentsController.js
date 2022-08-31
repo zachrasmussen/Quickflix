43fab5355bd12869490a735cf6aa29d13a73d32e
Kelly <kellysue3@yahoo.com>
2022-08-31 15:55:42 -0600

    Get and Post content

--- Quickflix/server/controllers/ContentsController.js
+++ Quickflix/server/controllers/ContentsController.js
@@ -1,4 +1,5 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
+import { contentService } from "../services/ContentsService";
 import BaseController from "../utils/BaseController";
 
 export class ContentsController extends BaseController {
@@ -7,14 +8,25 @@ export class ContentsController extends BaseController {
         this.router
         .use(Auth0Provider.getAuthorizedUserInfo)
         .post('', this.create)
+        .get('/:id', this.getContentByAccountId)
     }
-
-async create (req, res, next) {
-    try {
+    
+    async create (req, res, next) {
+      try {
         req.body.accountId = req.userInfo.id 
-      return res.send()
-    } catch (error) {
-      next(error)
+        let newContent=  await contentService.create(req.body)//body on request coming into server
+        return res.send(newContent)
+      } catch (error) {
+        next(error)
+      }
     }
-}
+    async getContentByAccountId(req, res, next) {
+      try {
+        const content = await contentService.getByContentByAccountId(req.params.id)
+        return res.send(content)
+      } catch (error) {
+        next(error)
+        
+      }
+    }
 }
--- Quickflix/server/services/ContentsService.js
+++ Quickflix/server/services/ContentsService.js
@@ -1,9 +1,13 @@
 import {dbContext} from "../db/DBContext.js"
 
 class ContentsService {
+    async getByContentByAccountId(id) {
+        const content = await dbContext.Content.findById(id)//Do we want to populate creator name and picture?
+        return content
+    }
     async create (newContent){
         const content = await dbContext.Content.create(newContent)
-    
+        return content  
     }
 }
 
