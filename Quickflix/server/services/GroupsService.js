7c14cc4a2c81210d056004c7bdcabfba392ba8c2
Zach Rasmussen <zsrasmussen@gmail.com>
2022-08-31 14:15:41 -0600

    Quickflix

--- Quickflix/server/services/GroupsService.js
+++ Quickflix/server/services/GroupsService.js
@@ -1,13 +1,24 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors"
+import { request } from "express"
 import { dbContext } from "../db/DbContext"
 
 
 class GroupsService {
 
-async create(newGroup){
-  const group = await dbContext.Groups.create(newGroup)
-  await group.populate('creator', 'name picture')
-  return group
-}
+  async create(newGroup) {
+    const group = await dbContext.Groups.create(newGroup)
+    await group.populate('creator', 'name picture')
+    return group
+  }
+
+  async getGroupById(id) {
+    const group = await dbContext.Groups.findById(id).populate('creator', 'name picture')
+    if (!group) {
+      throw new BadRequest('no group by that id')
+    }
+    return group
+  }
+
 
 }
 
