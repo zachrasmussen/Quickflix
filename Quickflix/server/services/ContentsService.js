e2761e89c12ff9b0fa0793c9c9983824b88ffcf2
Curtis Pollard <clp.the.second@gmail.com>
2022-09-01 09:31:05 -0600

    morning commit

--- Quickflix/server/services/ContentsService.js
+++ Quickflix/server/services/ContentsService.js
@@ -3,7 +3,7 @@ import {dbContext} from "../db/DBContext.js"
 class ContentsService {
     async create (newContent){
         const content = await dbContext.Content.create(newContent)
-    
+        return content
     }
 }
 
