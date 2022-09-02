020e2131a9d157230e0c4d9a37c8aa0569dbbdd3
Zach Rasmussen <zsrasmussen@gmail.com>
2022-09-02 16:20:56 -0600

    quickflix

--- Quickflix.client/src/pages/HomePage.vue
+++ Quickflix.client/src/pages/HomePage.vue
@@ -13,8 +13,8 @@
       />
       <h3 class="text-center my-2">{{ content.title }}</h3>
       <div class="d-flex justify-content-between mt-5">
-        <h1 class="mdi mdi-thumb-down selectable"></h1>
-        <h1 class="mdi mdi-thumb-up selectable"></h1>
+        <h1 class="bg-danger rounded-circle p-2">👍🏼</h1>
+        <h1 class="bg-success rounded-circle p-2">👎🏼</h1>
       </div>
     </div>
   </div>
