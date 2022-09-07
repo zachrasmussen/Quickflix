34330b7acb243e821a7856113ad17e9ea530b74b
Zach Rasmussen <zsrasmussen@gmail.com>
2022-09-07 11:14:00 -0600

    languageFilter In Service

--- Quickflix.client/src/services/ContentService.js
+++ Quickflix.client/src/services/ContentService.js
@@ -18,26 +18,29 @@ class ContentService {
         }
 
 
-      
+
         async createContent(content) {
 
                 const res = await api.post('api/content', content)
                 AppState.myContent = res.data
                 console.log('Created Content', res.data)
-
         }
 
         async updateGenreFilter(genre) {
                 AppState.filters.genre.push(genre)
                 console.log('filtered genres in the service', AppState.filters.genre)
-                
         }
 
-        async runFilter(){
-             const res = await ottApi.get('/?genre=' + AppState.filters.genre.join(","))
-             AppState.contents = res.data.results
+        async updateLangaugeFilter(language) {
+                AppState.filters.language.push(language)
+                console.log('filtered language in the service', AppState.filters.language)
         }
-      
+
+        async runFilter() {
+                const res = await ottApi.get('/?genre=' + AppState.filters.genre.join(",") && '/?langauge=' + AppState.filters.language.join(","))
+                AppState.contents = res.data.results
+        }
+
 
 
 
