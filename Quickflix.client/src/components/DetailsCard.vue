42a0af9b1e5026981dffaa0a6a44896867c53142
JD Simmons <hippeejd@gmail.com>
2022-09-09 15:00:12 -0600

    still working

--- Quickflix.client/src/components/DetailsCard.vue
+++ Quickflix.client/src/components/DetailsCard.vue
@@ -1,8 +1,13 @@
 <template>
 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <h1>HELLO</h1>
+    <div class="modal-content" >
+      <div class="text-center">
+     <h3>{{content.title}}</h3> <b>{{content.imbdrating}}</b>
+     <p>{{content.synopsis}}</p>
+    
+    
+    </div>
     </div>
   </div>
 </div>
@@ -10,11 +15,20 @@
 
 </template>
 <script>
+import { computed } from "@vue/reactivity";
+import { AppState } from "../AppState";
+
 export default {
+  
+  // props: {
+  //   content: {type: Object, required: true}
+  // },
 setup()
 
  {
-  return {};
+  return {
+    content: computed(() => AppState.contents[0]),
+  };
 },
 };
 </script>
