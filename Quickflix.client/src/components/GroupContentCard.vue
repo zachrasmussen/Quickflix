9b708d1e3f7be0e190f13e32e32fbc674bf581dd
Zach Rasmussen <zsrasmussen@gmail.com>
2022-09-11 21:01:56 -0600

    group content card styling

--- Quickflix.client/src/components/GroupContentCard.vue
+++ Quickflix.client/src/components/GroupContentCard.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="" v-if="groupContents">
-    <div class="d-flex justify-content-center">
-      <div class="col-md-3 m-3">
+    <div class="row justify-content-center d-flex">
+      <div class="col-10 col-md-4 m-3 p-0 card d-flex flex-row">
         <img
           class="customImg"
           :src="groupContents.imageurl"
           alt="Card image cap"
         />
-        <h6 class="">{{ groupContents.title }}</h6>
+        <div class="m-3">
+          <h6 class="font-title">{{ groupContents.title }}</h6>
+          <p>Genre: {{ groupContents.genre }}</p>
+          <p>Rating: {{ groupContents.imdbrating }}</p>
+          <p>Synopsis: {{ groupContents.synopsis }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -32,7 +37,11 @@ export default {
 
 <style>
 .customImg {
-  height: 600px;
-  width: 360px;
+  width: 150px;
+}
+.font-title {
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.05em;
+  font-weight: 700;
 }
 </style>
